c534e4e46d27cb74a105a2dc6663fcabf4c36b34
Jesus <jesus.tejedor.ovejas@gmail.com>
2018-05-01 15:24:37 +0200

    alumnos ordenados

---
+++ Frutas/src/Fruit.java
@@ -0,0 +1,24 @@
+
+public class Fruit {
+	String nombre;
+	String cantidad;
+	public Fruit(String nombre, String cantidad) {
+		super();
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+
+}
